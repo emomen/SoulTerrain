6939eae50334c1971107ee6d16f14ac1dfe573e9
iruiz10 <98486362+iruiz10@users.noreply.github.com>
2022-02-24 11:25:50 -0800

    Add files via upload

---
+++ iruiz.cpp
@@ -0,0 +1,11 @@
+//Isiah Ruiz
+//Source file for CMPS 3350 Project: Soulterrain
+//Last modified: February 24, 2022
+
+#include <cstdlib>
+
+int main()
+{
+    //todo: add functionality
+    return 0;
+}
