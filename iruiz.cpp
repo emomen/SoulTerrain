90a6fe41d9250ded50627a954c710fdb63931f3c
Isiah Ruiz <iruiz@odin.cs.csubak.edu>
2022-02-24 16:40:27 -0800

    Now prints something

--- iruiz.cpp
+++ iruiz.cpp
@@ -2,11 +2,11 @@
 //Source file for CMPS 3350 Project: Soulterrain
 //Last modified: February 24, 2022
 
-#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    //todo: add functionality
-    //
+    cout << "Hello world!" << endl;
     return 0;
 }
