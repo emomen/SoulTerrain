0d330b0605524c9edc844349d92226748a781221
Isiah Ruiz <iruiz@odin.cs.csubak.edu>
2022-04-07 11:22:54 -0700

    edited code style

--- iruiz.cpp
+++ iruiz.cpp
@@ -9,27 +9,35 @@ using namespace std;
 
 void test(char *keys)
 {
-    if (keys[XK_W] || keys[XK_w])
+    if (keys[XK_W] || keys[XK_w]) {
         cout << "W pressed" << endl;
-    if (keys[XK_A] || keys[XK_a])
+    }
+    if (keys[XK_A] || keys[XK_a]) {
         cout << "A pressed" << endl;
-    if (keys[XK_S] || keys[XK_s])
+    }
+    if (keys[XK_S] || keys[XK_s]) {
         cout << "S pressed" << endl;
-    if (keys[XK_D] || keys[XK_d])
+    }
+    if (keys[XK_D] || keys[XK_d]) {
         cout << "D pressed" << endl;
+    }
 }
 
 void iruiz(char *keys)
 {
     cout << "Testing diagonal directions. " << endl;
     cout << "Try to go in diagonal directions with WASD." << endl;
-    if((keys[XK_W] || keys[XK_w]) && (keys[XK_A] || keys[XK_a]))
+    if((keys[XK_W] || keys[XK_w]) && (keys[XK_A] || keys[XK_a])) {
         cout << "northwest " << endl;
-    if ((keys[XK_W] || keys[XK_w]) && (keys[XK_D] || keys[XK_d]))
+    }
+    if ((keys[XK_W] || keys[XK_w]) && (keys[XK_D] || keys[XK_d])) {
         cout << "northeast" << endl;
-    if ((keys[XK_S] || keys[XK_s]) && (keys[XK_A] || keys[XK_a]))
+    }
+    if ((keys[XK_S] || keys[XK_s]) && (keys[XK_A] || keys[XK_a])) {
         cout << "southwest" << endl;
-    if ((keys[XK_W] || keys[XK_w]) && (keys[XK_A] || keys[XK_a]))
+    }
+    if ((keys[XK_W] || keys[XK_w]) && (keys[XK_A] || keys[XK_a])) {
         cout << "southeast" << endl;
+    }
     else cout << "Not a diagonal direction." << endl;
 }
